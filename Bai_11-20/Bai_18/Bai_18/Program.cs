a4f3cf3a8b85f1eaf88865312777c6f6d889ab0b
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-03-16 20:47:43 +0700

    Thanh Nhan

---
+++ Bai_11-20/Bai_18/Bai_18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long h_242;
+            Console.Write("Nhap so gio: ");
+            h_242 = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("So tuan: " + (h_242 / (24 * 7)) + " tuan" );
+            Console.WriteLine("So ngay: " + (h_242 % (24 * 7)) / 24 + " ngay" );
+            Console.WriteLine("So gio: " + (h_242 % (24 * 7)) % 24 + " gio");
+
+            Console.ReadKey();
+        }
+    }
+}
