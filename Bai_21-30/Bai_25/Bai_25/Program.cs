f83503728a1d3b735b18c4047b41991a1f75ffb3
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-03-16 20:48:47 +0700

    Thanh Nhan

---
+++ Bai_21-30/Bai_25/Bai_25/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a_242, b_242, c_242, LCM_242;
+            Console.Write("Nhap a = ");
+            a_242 = int.Parse(Console.ReadLine());
+            Console.Write("Nhap b = ");
+            b_242 = int.Parse(Console.ReadLine());
+
+            c_242 = (a_242 * b_242);
+            
+            while (a_242 != b_242)
+            {
+                if (a_242 > b_242)
+                    a_242 -= b_242;
+                else
+                    b_242 -= a_242;
+            }
+            Console.Write("\nUSCLN = " + a_242);
+            LCM_242 = c_242 / a_242;
+            Console.Write("\nBSCNN = " + LCM_242);
+
+            Console.ReadKey();
+        }
+    }
+}
