cb7f95ddcea383d268afb687094373d268ef15d7
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-03-28 21:01:47 +0700

    Thanh Nhan

---
+++ In_Tam_Giac_Can_Rong/In_Tam_Giac_Can_Rong/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Tam_Giac_Can_Rong
+{
+    class Program
+    {
+        static void TamGiacCan(int n)
+        {
+            int k;
+            k = n - 1;
+            for(int i = 0; i < n - 1; i++)
+            {
+                for(int j = 0; j < 2 * n - 1; j++)
+                {
+                    if (j == k - i || j == k + i)
+                        Console.Write(" * ");
+                    else
+                        Console.Write("   ");
+                }
+                Console.Write("\n");
+            }
+            for(int i = 0; i < 2 * n - 1; i++)
+                Console.Write(" * ");
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap so bat ky: ");
+            n = int.Parse(Console.ReadLine());
+            TamGiacCan(n);
+
+            Console.ReadKey();
+        }
+    }
+}
