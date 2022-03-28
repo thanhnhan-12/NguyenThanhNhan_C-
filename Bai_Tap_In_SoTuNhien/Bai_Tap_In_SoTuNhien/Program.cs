f25d6b2077c7b89cd74619a12fff4d7787ccfe5d
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-03-28 20:58:43 +0700

    Thanh Nhan

---
+++ Bai_Tap_In_SoTuNhien/Bai_Tap_In_SoTuNhien/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_Tap_In_SoTuNhien
+{
+    class Program
+    {
+        static int Natural_Number(int n)
+        {
+            int S = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                S += i;
+            }
+            return S;
+        }
+
+        static void Main(string[] args)
+        {
+            int So;
+            Console.Write("Nhap so bat ky: ");
+            So = int.Parse(Console.ReadLine());
+            Console.Write("Tong cua so tu nhien la: " + Natural_Number(So));
+
+            Console.ReadKey();
+        }
+    }
+}
