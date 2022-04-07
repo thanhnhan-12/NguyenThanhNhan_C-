8f5e02f081549c511af8df482e36f9a0431c1ffc
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-04-07 20:04:06 +0700

    Thanh Nhan

---
+++ Bai_58/Bai_58/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bai_58
+{
+    class Program
+    {
+        static void Erastosthenes(int n_42)
+        {
+            bool[] check_42 = new bool[n_42 + 1];
+
+            for (int i_42 = 2; i_42 <= n_42; i_42++)
+            {
+                check_42[i_42] = true;
+            }
+
+            for (int i_42 = 2; i_42 <= n_42; i_42++)
+            {
+                if (check_42[i_42] == true)
+                {
+                    for (int j_42 = 2 * i_42; j_42 <= n_42; j_42 += i_42)
+                    {
+                        check_42[j_42] = false;
+                    }
+                }
+            }
+
+            for (int i_42 = 2; i_42 <= n_42; i_42++)
+            {
+                if (check_42[i_42] == true)
+                {
+                    Console.Write(" " + i_42);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n_42;
+            Console.Write("Nhap so bat ky: ");
+            n_42 = int.Parse(Console.ReadLine());
+            
+            Erastosthenes(n_42);
+
+            Console.ReadKey();
+        }
+    }
+}
