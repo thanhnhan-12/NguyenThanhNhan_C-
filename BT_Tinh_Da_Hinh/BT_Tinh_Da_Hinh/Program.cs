c082a12b434013f584066f91fa4d8cbc541305eb
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-04-23 20:57:36 +0700

    Thanh Nhan

--- BT_Tinh_Da_Hinh/BT_Tinh_Da_Hinh/Program.cs
+++ BT_Tinh_Da_Hinh/BT_Tinh_Da_Hinh/Program.cs
@@ -9,18 +9,9 @@ namespace BT_Tinh_Da_Hinh
     class Program
     {
         class Triangle_Quadrilateral
-        {           
-            public void Input()
-            {
-
-            }
-
-            public void Output()
-            {
-
-            }
-
-            public void LuaChon()
+        {
+            
+            public virtual void Select()
             {
 
             }
@@ -29,6 +20,44 @@ namespace BT_Tinh_Da_Hinh
 
         class Triangle : Triangle_Quadrilateral
         {
+            int Side1_242, Side2_242, Side3_242;
+            public void Input()         // Nhập cạnh tam giác
+            {
+                Console.Write("Nhap canh thu nhat: ");
+                Side1_242 = int.Parse(Console.ReadLine());
+                Console.Write("Nhap canh thu hai: ");
+                Side2_242 = int.Parse(Console.ReadLine());
+                Console.Write("Nhap canh thu ba: ");
+                Side3_242 = int.Parse(Console.ReadLine());
+            }
+
+            public void Output()        // In ra cạnh tam giác
+            {
+                Console.Write(" " + Side1_242);
+                Console.Write("\n " + Side2_242);
+                Console.Write("\n " + Side3_242);
+            }
+
+            public void Test()
+            {
+                if ((Side1_242 + Side2_242) > Side3_242 && (Side1_242 + Side3_242) > Side2_242 &&
+                   (Side2_242 + Side3_242) > Side1_242)
+                {
+                    Console.Write("\nDay la Hinh tam giac ");
+                }
+                else
+                {
+                    Console.Write("\nKhong phai hinh tam giac ");
+                    Console.WriteLine("\nMoi nhap lai! ");
+                    Input();
+                    Output();
+                    if ((Side1_242 + Side2_242) > Side3_242 && (Side1_242 + Side3_242) > Side2_242 &&
+                        (Side2_242 + Side3_242) > Side1_242)
+                    {
+                        Console.Write("\nDay la Hinh tam giac ");
+                    }
+                }
+            }
 
         }
 
@@ -49,7 +78,10 @@ namespace BT_Tinh_Da_Hinh
 
         static void Main(string[] args)
         {
-
+            Triangle Tag = new Triangle();
+            Tag.Input();
+            Tag.Output();
+            Tag.Test();
 
             Console.ReadKey();
         }
