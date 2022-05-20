a126eab1821ec7b2cfcd7aa1d1c6ec4005c684e7
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-05-20 19:58:35 +0700

    Thanh Nhan

--- Residential/NguyenThanhNhan_1911505310242/NguyenThanhNhan_1911505310242/Program.cs
+++ Residential/NguyenThanhNhan_1911505310242/NguyenThanhNhan_1911505310242/Program.cs
@@ -15,7 +15,43 @@ namespace NguyenThanhNhan_1911505310242
         private string nghenghiep_242;
         private int cmnd_242;
 
+        public Person()
+        {
 
+        }
+
+        public Person(string hoten_242, int tuoi_242, string nghenghiep_242, int cmnd_242)
+        {
+            this.Hoten_242 = hoten_242;
+            this.Tuoi_242 = tuoi_242;
+            this.Nghenghiep_242 = nghenghiep_242;
+            this.Cmnd_242 = cmnd_242;
+        }
+
+        public string Hoten_242 { get => hoten_242; set => hoten_242 = value; }
+        public int Tuoi_242 { get => tuoi_242; set => tuoi_242 = value; }
+        public string Nghenghiep_242 { get => nghenghiep_242; set => nghenghiep_242 = value; }
+        public int Cmnd_242 { get => cmnd_242; set => cmnd_242 = value; }
+
+        public virtual void Input()
+        {
+            Console.Write("Nhap ho va ten: ");
+            hoten_242 = Console.ReadLine();
+            Console.Write("Nhap tuoi: ");
+            tuoi_242 = Int32.Parse(Console.ReadLine());
+            Console.Write("Nhap nghe nghiep: ");
+            nghenghiep_242 = Console.ReadLine();
+            Console.Write("Nhap so CMND: ");
+            cmnd_242 = Int32.Parse(Console.ReadLine());
+        }
+
+        public virtual void Output()
+        {
+            Console.Write("Ho ten: " + hoten_242);
+            Console.Write("Tuoi: " + tuoi_242);
+            Console.Write("Nghe nghiep: " + nghenghiep_242);
+            Console.Write("CMND: " + cmnd_242);
+        }
 
     }
 
