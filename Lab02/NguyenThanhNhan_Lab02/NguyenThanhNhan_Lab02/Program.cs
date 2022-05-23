db459c1dc5e80915c36959df1ad2f7ecb04096b2
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-05-23 21:17:03 +0700

    Thanh Nhan

--- Lab02/NguyenThanhNhan_Lab02/NguyenThanhNhan_Lab02/Program.cs
+++ Lab02/NguyenThanhNhan_Lab02/NguyenThanhNhan_Lab02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,78 @@ using System.Threading.Tasks;
 
 namespace NguyenThanhNhan_Lab02
 {
+    interface IBook
+    {
+        string this[int index_242]
+        {
+            get;
+            set;
+        }
+
+        string Title_242
+        {
+            get;
+            set;
+        }
+
+        string Author_242
+        {
+            get;
+            set;
+        }
+
+        string Publisher_242
+        {
+            get;
+            set;
+        }
+
+        string ISBN_242
+        {
+            get;
+            set;
+        }
+
+        int Year_242
+        {
+            get;
+            set;
+        }
+
+        void Show();
+    }
+
+    class Book : IBook
+    {
+        private string isbn_242;
+        private string title_242;
+        private string author_242;
+        private string publisher_242;
+        private int year_242;
+
+        private ArrayList chapter_242 = new ArrayList();
+
+        public string this[int index_242] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Title_242 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Author_242 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Publisher_242 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ISBN_242 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Year_242 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
+
+            Console.ReadKey();
         }
     }
 }
