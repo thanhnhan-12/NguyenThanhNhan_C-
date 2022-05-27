0ce24c47fe3199605913bd3cb14feaf1835dec79
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2022-05-27 13:58:33 +0700

    Thanh Nhan

--- Manager_Officer_Bai_1/Manager_Officer/Manager_Officer/Program.cs
+++ Manager_Officer_Bai_1/Manager_Officer/Manager_Officer/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Manager_Officer
 {
+    /* Cán bộ */
     class CanBo
     {
         private string hoTen_242;
@@ -30,7 +31,31 @@ namespace Manager_Officer
         public int Tuoi_242 { get => tuoi_242; set => tuoi_242 = value; }
         public string GioiTinh_242 { get => gioiTinh_242; set => gioiTinh_242 = value; }
         public string DiaChi_242 { get => diaChi_242; set => diaChi_242 = value; }
+
+        public virtual void Nhap_CanBo()
+        {
+            Console.Write("\nNhap Ho va Ten : ");
+            hoTen_242 = Console.ReadLine();
+            Console.Write("Nhap tuoi: ");
+            tuoi_242 = Int32.Parse(Console.ReadLine());
+            Console.Write("Nhap gioi tinh: ");
+            gioiTinh_242 = Console.ReadLine();
+            Console.Write("Nhap dia chi: ");
+            diaChi_242 = Console.ReadLine();
+        }
+
+        public virtual void Xuat_CanBo()
+        {
+            Console.Write("\n---------------------------");
+            Console.Write("\nHo va ten: " + hoTen_242);
+            Console.Write("\nTuoi: " + tuoi_242);
+            Console.Write("\nGioi tinh: " + gioiTinh_242);
+            Console.Write("\nDia chi: " + diaChi_242);
+        }
+
     }
+
+    /* Công nhân */
 
     class Program
     {
